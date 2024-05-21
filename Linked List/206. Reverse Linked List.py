18bbfee786f1afb3426d1b0f948ead957e931e2f
Kiran Vignesh <rajkiranvignesh369@gmail.com>
2024-05-21 13:35:32 +0530

    May 21 - 24

--- Linked List/206. Reverse Linked List.py
+++ Linked List/206. Reverse Linked List.py
@@ -11,6 +11,7 @@ class Solution(object):
 
         Approach:
         create two nodes - prev, curr = None, head... while curr: temp = (1) curr.next, (2) curr.next = prev, (3) prev = curr, (4) curr = temp... finally return prev (which is now the head)
+        time: O(n) --- space: O(1)
         """
         prev, curr = None, head
 
